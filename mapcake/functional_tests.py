b8a27422e8ec051cf756b213e7a797d834897d70
David Martínez Morata <David@MacBook-Pro-de-David.local>
2015-03-04 17:28:14 +0100

    First Functional Test with unittest

--- mapcake/functional_tests.py
+++ mapcake/functional_tests.py
@@ -1,7 +1,29 @@
 # -*- coding:utf-8 -*-
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    """docstring for NewVisitorTest"""
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_visit_the_homepage(self):
+        # Thales was listen last nigth about a new cool map service named
+        # MapCake. He goes to the home page to view the homepage
+        self.browser.get('http://localhost:8000')
+
+        # He show the page title and header mention maps and MapCake
+        self.assertIn('MapCake', self.browser.title)
+        self.fail('Finish the test')
+
+        # Satisfied, he goes out to meet some friends
+
+if __name__ == '__main__':
+    unittest.main()
