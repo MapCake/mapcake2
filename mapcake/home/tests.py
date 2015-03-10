a3b8b11340ff1bd9716b9c8af3f085e5f8352afe
David Martínez Morata <David@MacBook-Pro-de-David.local>
2015-03-10 11:34:37 +0100

    Test Tille shows MapCake in home page

---
+++ mapcake/home/tests.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.core.urlresolvers import reverse
+from home.views import home
+
+# Create your tests here.
+
+
+class HomePageTest(TestCase):
+
+    """docstring for TestHomePage"""
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+
+    def test_uses_index_template(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home/index.html")
+
+    def test_uses_base_template(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "base.html")
